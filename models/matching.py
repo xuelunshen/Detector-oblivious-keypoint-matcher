d3bd0559cf5eeac07113a65e14c3b74414a740d7
Lloyd <xuelun@stu.xmu.edu.cn>
2021-04-11 13:35:59 +0800

    Update R2D2-SP-SP

---
+++ models/matching.py
@@ -0,0 +1,118 @@
+# %BANNER_BEGIN%
+# ---------------------------------------------------------------------
+# %COPYRIGHT_BEGIN%
+#
+#  Magic Leap, Inc. ("COMPANY") CONFIDENTIAL
+#
+#  Unpublished Copyright (c) 2020
+#  Magic Leap, Inc., All Rights Reserved.
+#
+# NOTICE:  All information contained herein is, and remains the property
+# of COMPANY. The intellectual and technical concepts contained herein
+# are proprietary to COMPANY and may be covered by U.S. and Foreign
+# Patents, patents in process, and are protected by trade secret or
+# copyright law.  Dissemination of this information or reproduction of
+# this material is strictly forbidden unless prior written permission is
+# obtained from COMPANY.  Access to the source code contained herein is
+# hereby forbidden to anyone except current COMPANY employees, managers
+# or contractors who have executed Confidentiality and Non-disclosure
+# agreements explicitly covering such access.
+#
+# The copyright notice above does not evidence any actual or intended
+# publication or disclosure  of  this source code, which includes
+# information that is confidential and/or proprietary, and is a trade
+# secret, of  COMPANY.   ANY REPRODUCTION, MODIFICATION, DISTRIBUTION,
+# PUBLIC  PERFORMANCE, OR PUBLIC DISPLAY OF OR THROUGH USE  OF THIS
+# SOURCE CODE  WITHOUT THE EXPRESS WRITTEN CONSENT OF COMPANY IS
+# STRICTLY PROHIBITED, AND IN VIOLATION OF APPLICABLE LAWS AND
+# INTERNATIONAL TREATIES.  THE RECEIPT OR POSSESSION OF  THIS SOURCE
+# CODE AND/OR RELATED INFORMATION DOES NOT CONVEY OR IMPLY ANY RIGHTS
+# TO REPRODUCE, DISCLOSE OR DISTRIBUTE ITS CONTENTS, OR TO MANUFACTURE,
+# USE, OR SELL ANYTHING THAT IT  MAY DESCRIBE, IN WHOLE OR IN PART.
+#
+# %COPYRIGHT_END%
+# ----------------------------------------------------------------------
+# %AUTHORS_BEGIN%
+#
+#  Originating Authors: Paul-Edouard Sarlin
+#
+# %AUTHORS_END%
+# --------------------------------------------------------------------*/
+# %BANNER_END%
+
+import torch
+import torchvision.transforms as tvf
+
+from os.path import join, dirname
+
+from models.extract import load_network, NonMaxSuppression, extract_multiscale
+from .superpoint import SuperPoint
+from .superglue import SuperGlue
+
+RGB_mean = [0.485, 0.456, 0.406]
+RGB_std  = [0.229, 0.224, 0.225]
+norm_RGB = tvf.Compose([tvf.ToTensor(), tvf.Normalize(mean=RGB_mean, std=RGB_std)])
+
+
+class Matching(torch.nn.Module):
+    """ Image Matching Frontend (SuperPoint + SuperGlue) """
+    def __init__(self, config=None):
+        super().__init__()
+        if config is None:
+            config = {}
+        self.superpoint = SuperPoint(config.get('superpoint', {}))
+        self.superglue = SuperGlue(config.get('superglue', {}))
+        self.net = load_network(join(dirname(__file__), 'weights/r2d2_WASF_N16.pt'))
+        self.detector = NonMaxSuppression(rel_thr=0.7, rep_thr=0.7)
+
+    def forward(self, data):
+        """ Run SuperPoint (optionally) and SuperGlue
+        SuperPoint is skipped if ['keypoints0', 'keypoints1'] exist in input
+        Args:
+          data: dictionary with minimal keys: ['image0', 'image1']
+        """
+        pred = {}
+
+        def detect(data):
+            img = data['image']
+            img = norm_RGB(img/255.)[None]
+            img = img.cuda()
+            xys, desc, scores = extract_multiscale(self.net, img, self.detector,
+                                                   scale_f=2**0.25,
+                                                   min_scale=0,
+                                                   max_scale=1,
+                                                   min_size=256,
+                                                   max_size=1024,
+                                                   verbose=False)
+            data['keypoints'] = xys[:, :2][None].cuda()
+            return data
+
+        # Extract SuperPoint (keypoints, scores, descriptors) if not provided
+        if 'descriptors0' not in data:
+            data['image'] = data['img0']
+            data = detect(data)
+            data['image'] = data['image0']
+            pred0 = self.superpoint(data)
+            pred = {**pred, **{k+'0': v for k, v in pred0.items()}}
+        if 'descriptors1' not in data:
+            data['image'] = data['img1']
+            data = detect(data)
+            data['image'] = data['image1']
+            pred1 = self.superpoint(data)
+            pred = {**pred, **{k+'1': v for k, v in pred1.items()}}
+
+        data.pop('image')
+        data.pop('keypoints')
+        # Batch all features
+        # We should either have i) one image per batch, or
+        # ii) the same number of local features for all images in the batch.
+        data = {**data, **pred}
+
+        for k in data:
+            if isinstance(data[k], (list, tuple)):
+                data[k] = torch.stack(data[k])
+
+        # Perform the matching
+        pred = {**pred, **self.superglue(data)}
+
+        return pred
